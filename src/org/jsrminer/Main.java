d4462b819840cb6c3d18bb5f3e72c3b9a07833c9
shiblu <mosabbir.shiblu@gmail.com>
2020-01-09 18:29:57 -0500

    Add log4j2 logger and gitservice based on RMiner

---
+++ src/org/jsrminer/Main.java
@@ -0,0 +1,28 @@
+package org.jsrminer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.jsrminer.api.GitService;
+import org.jsrminer.util.JGitService;
+
+public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        log.info("Info");
+        log.debug("Program Starts");
+        GitService git = new JGitService();
+        try {
+            Repository repo = git.cloneIfNotExists(
+                    "tmp/refactoring-toy-example",
+                    "https://github.com/danilofes/refactoring-toy-example.git");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        log.info("-----------------Program Ends--------------------");
+    }
+}
