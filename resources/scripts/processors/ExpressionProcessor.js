e67c199d9c14f7c4438c798340060366552e7e1b
Mosabbir Khan Shiblu <mkshiblu@gmail.com>
2020-08-28 20:56:24 -0400

    no message

--- resources/scripts/processors/ExpressionProcessor.js
+++ resources/scripts/processors/ExpressionProcessor.js
@@ -1,18 +1,50 @@
+const processes = new Map([
+    ['BinaryExpression', processBinaryExpression],
+    ['Identifier', processIdentifier],
+]);
+
 /**
  * Any expression node. 
  * Since the left-hand side of an assignment may be any expression in general, 
  * an expression can also be a pattern.
  * @param {*} node 
  */
-function processExpression(node) {
-    switch (condition.type) {
-        case 'BinaryExpression':
-            const left = condition.left;
-            const operator = condition.operator;
-            const right = condition.right;
-            processExpression(right);
-            break;
+function processExpression(path) {
+    const node = path.node;
+    const process = processes.get(nodePath.node.type);
+    if (process) {
+    } else {
+        throw 'Processeor not implemented for : ' + path.node.type;
     }
+}
+
+/**
+interface BinaryExpression<: Expression {
+    type: "BinaryExpression";
+    operator: BinaryOperator;
+    left: Expression;
+    right: Expression;
+}
+ */
+function processBinaryExpression(path) {
+    const node = path.node;
+    const left = node.left;
+    const operator = node.operator;
+    const right = node.right;
+    processExpression(left);
+    processExpression(right);
+}
+
+/**
+ * interface Identifier <: Expression, Pattern {
+  type: "Identifier";
+  name: string;
+}
+An identifier. Note that an identifier may be an expression or a destructuring pattern
+ * @param {*} path 
+ */
+function processIdentifier(path) {
+    const name = path.node.name;
 }
 
 exports.processExpression = processExpression;
