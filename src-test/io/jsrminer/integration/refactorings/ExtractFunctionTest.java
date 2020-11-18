cb12395841cd16a3638011f465784716b6351734
shiblu <mosabbir.shiblu@gmail.com>
2020-11-18 16:49:01 -0500

    #37: Add Junit 5 for testing

---
+++ src-test/io/jsrminer/integration/refactorings/ExtractFunctionTest.java
@@ -0,0 +1,13 @@
+package io.jsrminer.integration.refactorings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExtractFunctionTest {
+
+    @Test
+    void name() {
+        assertTrue("x".length() == 1);
+    }
+}
