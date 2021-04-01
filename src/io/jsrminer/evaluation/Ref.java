4d7386a314eeef9cb9df87d8805415da195d2257
Mosabbir Khan Shiblu <mkshiblu@gmail.com>
2021-04-01 18:10:31 -0400

    Added source location with line column in evaluator

--- src/io/jsrminer/evaluation/Ref.java
+++ src/io/jsrminer/evaluation/Ref.java
@@ -83,15 +83,27 @@ public class Ref {
             fromStringMap.put("RENAME_VARIABLE", RENAME_VARIABLE);
         }
     }
-
-
+     
     private SourceLocation toSourceLocation(String location) {
         var splitted = location.split(":");
         String filePath = splitted[0];
-        var startEndSplitted = splitted[1].split("-");
+
+        var segments = splitted[1].split("|");
+        var startEndSplitted = segments[0].split("-");
         var start = Integer.parseInt(startEndSplitted[0]);
         var end = Integer.parseInt(startEndSplitted[1]);
-        return new SourceLocation(filePath, 0, 0, 0, 0, start, end);
+
+        var lineColSplitted = segments[1].split("-");
+        var startLineColSplitted = lineColSplitted[0].substring(1, lineColSplitted[0].length() - 1).split(",");
+        var endLineColSplitted = lineColSplitted[1].substring(1, lineColSplitted[1].length() - 1).split(",");
+
+        var startLine = Integer.parseInt(startLineColSplitted[0]);
+        var startColumn = Integer.parseInt(startLineColSplitted[1]);
+
+        var endLine = Integer.parseInt(endLineColSplitted[0]);
+        var endColumn = Integer.parseInt(endLineColSplitted[1]);
+
+        return new SourceLocation(filePath, startLine, startColumn, endLine, endColumn, start, end);
     }
 
     public void setLocationBefore(String location) {
