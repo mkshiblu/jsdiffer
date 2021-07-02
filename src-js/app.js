8303ba3000534a2452c46fcce282f938cfa1295b
Mosabbir Khan Shiblu <mkshiblu@gmail.com>
2021-07-02 18:06:09 -0400

    Fixed babel generator not found issue

--- src-js/app.js
+++ src-js/app.js
@@ -1,24 +1,7 @@
-const { parse } = require('@babel/parser');
-
-const generator = require('@babel/generator');
 const parser = require('./babel_parser');
 
-let content = `
-var x = new Function () {  };
-`;
-
+let content = 'var x = function() {  }';
 const ast = parser.parse(content);
 const formattedCode = parser.format(ast, true);
-//const formattedAst = parser.parse(formattedCode);
-console.log(formattedCode);
-
-exports.format = function format(node, appendSemicolon = false) {
-  return generator.default(node, {
-    comments: false,
-    concise: true,
-    //sourceFileName: 'dsa',
-
-    //sourceMaps: true,
-    //retainLines: true,
-  }).code;
-};
+const formattedAst = parser.parse(formattedCode);
+console.log(formattedAst);
